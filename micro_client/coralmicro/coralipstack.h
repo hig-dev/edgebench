b721370ace07bb75d18d575a1525f9ee89b2ff77
Halil Göcer <dev@hig.dev>
2025-05-30 13:03:59 +0200

    Improve and simplify ip stack

--- micro_client/coralmicro/coralipstack.h
+++ micro_client/coralmicro/coralipstack.h
@@ -1,8 +1,6 @@
 #if !defined(CORALIPSTACK_H)
 #define CORALIPSTACK_H
 
-#include "third_party/freertos_kernel/include/FreeRTOS.h"
-#include "third_party/freertos_kernel/include/task.h"
 #include "coralmicro/libs/base/network.h"
 
 class CoralIPStack
@@ -30,7 +28,7 @@ public:
         return sockfd_;
     }
 
-    /* returns the number of bytes read, which could be 0.
+    /* returns the number of bytes read, which could be 0 (timeout).
        -1 if there was an error on the socket
     */
     int read(unsigned char *buffer, int len, int timeout)
@@ -40,40 +38,20 @@ public:
             printf("SocketClient not connected\r\n");
             return -1; // not connected
         }
-        int interval = 10;  // all times are in milliseconds
-		int total = 0;
-
-		if (timeout < 30)
+        coralmicro::IOStatus ioStatus = coralmicro::ReadBytes(sockfd_, buffer, len);
+        switch (ioStatus)
         {
-            interval = 2;
-        }
-		
-        while (coralmicro::SocketAvailable(sockfd_) < len && total < timeout)
-		{
-			vTaskDelay(interval / portTICK_PERIOD_MS);
-			total += interval;
-		}
-		if (coralmicro::SocketAvailable(sockfd_) >= len)
-		{
-            auto ioStatus = coralmicro::ReadBytes(sockfd_, buffer, len);
-            switch (ioStatus)
-            {
-                case coralmicro::IOStatus::kOk:
-                    return len; // all bytes read
-                    break;
-                case coralmicro::IOStatus::kEof:
-                    printf("SocketClient EOF\r\n");
-                    return -1; // EOF means no more data to read
-                case coralmicro::IOStatus::kError:
-                    printf("SocketClient Error\r\n");
-                    return -1; // error reading from socket
-                default:
-                    printf("SocketClient Unknown IOStatus\r\n");
-                    return -1; // unknown status
-            }
-        }
-		else {
-            return 0; // no data available due to timeout
+            case coralmicro::IOStatus::kOk:
+                return len;
+            case coralmicro::IOStatus::kEof:
+                printf("SocketClient EOF\r\n");
+                return -1; // EOF means no more data to read
+            case coralmicro::IOStatus::kError:
+                printf("SocketClient Error\r\n");
+                return -1; // error reading from socket
+            default:
+                printf("SocketClient Unknown IOStatus\r\n");
+                return -1; // unknown status
         }
     }
 
